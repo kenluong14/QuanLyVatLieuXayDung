f051c5c495f3537e71aa98d83410af1f0ca67832
Trung <thanhtrung028473@gmail.com>
2020-10-23 10:34:33 +0700

    login

--- src/main/java/com/example/demo/constructionmaterialsmanagement/ConstructionMaterialsManagementApplication.java
+++ src/main/java/com/example/demo/constructionmaterialsmanagement/ConstructionMaterialsManagementApplication.java
@@ -8,7 +8,6 @@ public class ConstructionMaterialsManagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConstructionMaterialsManagementApplication.class, args);
-        //ok
     }
 
 }
---
+++ src/main/java/com/example/demo/constructionmaterialsmanagement/login.java
@@ -0,0 +1,4 @@
+package com.example.demo.constructionmaterialsmanagement;
+
+public class login {
+}
